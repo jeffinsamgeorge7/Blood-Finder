0045f744de805f75b3e4526059c69b9b1ff1da88
Jayalakshmi M <141710415+Jayalakshmi721@users.noreply.github.com>
2023-11-24 15:03:21 +0530

    Update MyBloodpost.java

--- main/java/com/example/blood_point/MyBloodpost.java
+++ main/java/com/example/blood_point/MyBloodpost.java
@@ -82,7 +82,8 @@ public class MyBloodpost extends AppCompatActivity {
                     L2.setVisibility(View.GONE);
                     L3.setVisibility(View.GONE);
                     L4.setVisibility(View.GONE);
-                    
+                    L5.setVisibility(View.GONE);
+                    No_post.setText("YOUR BLOOD REQUEST IS NOT BEING POSTED !");
                 }
             }
             @Override
